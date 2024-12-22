9c4476b956026fcb2afc74438d4b4d4fe2e3b01c
BeyondESH <1989601704@qq.com>
2024-12-22 22:53:24 +0800

    2

--- GateServer/cmake-build-debug/cmake_install.cmake
+++ GateServer/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Beyond/CLionProjects/GateServer
+# Install script for directory: C:/Users/Beyond/Documents/GitHub/WeChat/GateServer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -47,6 +47,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Beyond/CLionProjects/GateServer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Beyond/Documents/GitHub/WeChat/GateServer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
